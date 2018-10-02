8cf9eb6163d02bd570901d4f506c6dcde3796324
George Ward <gcward18@gmail.com>
2018-10-01 22:07:58 -0500

    finished HW3

--- HW3/_program/main.cpp
+++ HW3/_program/main.cpp
@@ -3,6 +3,7 @@
 #include "string.h"
 #include <utility>
 #include <thread>
+#include <algorithm>
 #include <vector>
 #include <chrono>
 #include <fstream>
@@ -10,6 +11,7 @@
 
 std::mutex word_count_lock;
 std::mutex file_queue_lock;
+bool sum_greater_geq_2 = false;
 
 void get_files(std::queue<std::string>& file_queue)
 {
@@ -41,12 +43,13 @@ void open_file(const std::string file_name, const std::string word, int& word_co
 
     // let user know if file was not able to be opened
     if(!inFile){
-        std::cout << word << " was not able to be opened" << std::endl;
+        std::cout << file_name << " was not able to be opened" << std::endl;
     }
     else{
         // iterate through the file to find matching words
         while(inFile>>content)
         {
+            // transform(content.begin(), content.end(), content.begin(), ::tolower);
             // found a match increment word count
             if(content == word){
 
@@ -55,48 +58,6 @@ void open_file(const std::string file_name, const std::string word, int& word_co
         }
     }
 }
-
-
-    /**
-     * Structure:
-     *
-     *  mapper thread -
-     *      FIRST
-     *          1.  lock file queue
-     *          2.  remove top file
-     *          3.  unlock file queue
-     *      SECOND
-     *          1.  Open file
-     *          2.  Count number of times word appears in
-     *              file
-     *          3.  Close file
-     *      THIRD
-     *          1.  lock count queue
-     *          2.  push sum onto the queue
-     *          3.  unlock count queue
-     *
-     *  reducer thread -
-     *      FIRST
-     *          1.  Lock count queue
-     *          2.  Remove top two elements
-     *          3.  Unlock the count queue
-     *      SECOND
-     *          1.  Sum two elements together
-     *      THIRD
-     *          1.  Lock count queue
-     *          2.  Push sum onto the count queue
-     *          3.  Unlock count queue
-     *
-     *  main function -
-     *          1.  prompt user for the word they wish to
-     *              count
-     *          2.  set up queue's
-     *          3.  launch mapper threads and wait for them
-     *              to finish counting
-     *          4.  launch reducer threads and wait for them
-     *              to finish their work
-     *          5.  print the total count
-     */
 
 void map(std::queue<std::string>& file_queue, std::queue<int>& word_count, std::string word){
 
@@ -112,7 +73,9 @@ void map(std::queue<std::string>& file_queue, std::queue<int>& word_count, std::
 
     word_count_lock.lock();
     word_count.push(sum);
+    std::cout << file_queue.front() << ": " << sum << std::endl;
     word_count_lock.unlock();
+    if(word_count.size() >= 2 ) sum_greater_geq_2 = true;
 
 }
 
@@ -125,28 +88,33 @@ void reduce(std::queue<int>& word_count, int i){
     // Reducer thread first job which is in the critical section.
     // Need to get the values from the word count queue and pop the
     // the values from the queue
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    while(sum_greater_geq_2 == false){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    if(word_count.size() < 2)
+    {
 
-    if(word_count.size() >= 0){
-        // get two elements from the queue
-        word_count_lock.lock();
-        first = word_count.front();
-        word_count.pop();
-        second = word_count.front();
-        word_count.pop();
-        // word_count_lock.unlock();
-        
-        // add the values together
-        result = first + second;
+    }
+    else
+        while(word_count.size() >= 2){
 
-        
-        // if (word_count.size() <= 10 && word_count.size() >= 0 ){
-        //     // // push the result back on the queue
-        //     word_count_lock.lock();
+            // get two elements from the queue
+            word_count_lock.lock();
+            first = word_count.front();
+            word_count.pop();
+            second = word_count.front();
+            word_count.pop();
+            word_count_lock.unlock();
+
+            // add the values together
+            result = first + second;
+
+            
+            // push the result back on the queue
+            word_count_lock.lock();
             word_count.push(result);
             word_count_lock.unlock();
-        // }
-    }
+        }
 }
 
 int main()
@@ -156,40 +124,58 @@ int main()
     std::queue<std::string> file_queue;
     std::queue<int> word_count;
     int size_of_file_queue = 0;
+    int num_reducers = 0;
     std::vector<std::thread> mapper_threads;
     std::vector<std::thread> reducer_threads;
+    std::string input = "";
+    
 
     int first   = 0;
     int second  = 0;
+        std::cout << "Please enter a word that you'd wish to count in our collection of books: ";
+        std::cin >> input;
 
+    while(input == ""){
+        std::cout << "Invalid input please try another word: ";
+        std::cin >> input;
+    }
+
+    std::cout << "Please enter the number of reducers you'd like to create: ";
+    std::cin >> num_reducers;
+
+    while(num_reducers == 0 || num_reducers > 80){
+        std::cout << "Invalid input please try another number: ";
+        std::cin >>num_reducers;
+    }
+
+
+    // transform(input.begin(), input.end(), input.begin(), ::tolower);
     // get the files that will be proccessed by the thread
     get_files(file_queue);
 
     size_of_file_queue = file_queue.size();
 
     for(unsigned int i = 0; i < size_of_file_queue; i++){
-        mapper_threads.push_back(std::thread(map, std::ref(file_queue), std::ref(word_count),"the"));
+        mapper_threads.push_back(std::thread(map, std::ref(file_queue), std::ref(word_count), input));
+    }
+
+    for(unsigned int i = 0; i < num_reducers; i++){
         reducer_threads.push_back(std::thread(reduce, std::ref(word_count), i));
     }
 
+    // Mappers and Reducers are running similatiously until this point. 
     for(auto& map : mapper_threads)
         map.join();
-    
-    for(auto& reduce: reducer_threads)
+
+    // Reducers are forced to end their process. 
+    for(auto& reduce : reducer_threads)
         reduce.join();
     
 
 
-    std::cout << "\"the\" appeared ";
+    std::cout << "\""<< input << "\" appeared ";
     std::cout << word_count.front();
     std::cout << " times" << std::endl;
 
-    // for(unsigned int i = 0; i < size_of_file_queue; i++){
-    //     std::cout << word_count.front() << ", ";
-    //     word_count.pop();    
-    // }
-    // std::cout << file_queue.front() << ", ";
-
-    // std::cout << std::endl;
     return 0;
 }
